c5502e2b30e5fdcb75ab95f9c4cd2d26b8806d9f
sarkanyjudo <sarkanyjudo@gmail.com>
2020-01-20 01:30:31 -1000

    Removing traced svg images

--- src/components/Tours/Tours.js
+++ src/components/Tours/Tours.js
@@ -1,36 +1,31 @@
-import React from 'react'
-import TourList from './TourList'
-import { useStaticQuery,graphql } from 'gatsby'
+import React from 'react';
+import TourList from './TourList';
+import { useStaticQuery, graphql } from 'gatsby';
 
-const getTours = graphql `
-    query {
-        tours:allContentfulTour {
-            edges {
-                node {
-                    name
-                    price
-                    slug
-                    country
-                    contentful_id
-                    days
-                    images {
-                        fluid {
-                            ...GatsbyContentfulFluid_tracedSVG
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-`
+const getTours = graphql`
+	query {
+		tours: allContentfulTour {
+			edges {
+				node {
+					name
+					price
+					slug
+					country
+					contentful_id
+					days
+					images {
+						fluid {
+							...GatsbyContentfulFluid
+						}
+					}
+				}
+			}
+		}
+	}
+`;
 const Tours = () => {
-    const {tours} = useStaticQuery(getTours);
-    return (
-        
-        <TourList tours={tours} />
-       
-    )
-}
+	const { tours } = useStaticQuery(getTours);
+	return <TourList tours={tours} />;
+};
 
-export default Tours
+export default Tours;
