e086f4182000a721d94608475c32eab4c46670ac
sarkanyjudo <sarkanyjudo@gmail.com>
2020-01-19 23:54:08 -1000

    Added blog posts, and blog list

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,41 +3,42 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`
+});
 
 module.exports = {
-  /* Your site config here */
-  siteMetadata: {
-    title:"Backroads",
-    description: "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure",
-    author: "@johndoe",
-    data: {
-      name: "john",
-      age: 24,
-    },
-  },
+	/* Your site config here */
+	siteMetadata: {
+		title: 'Backroads',
+		description:
+			'Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure',
+		author: '@johndoe',
+		twitterUsername: '@john_smilga',
+		image: '/defaultBcg.jpeg',
+		siteUrl: ''
+	},
 
-  plugins: [
-    `gatsby-plugin-styled-components`,
-    `gatsby-transformer-sharp`, 
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-transition-link`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
-    },
-  ]
-}
+	plugins: [
+		`gatsby-plugin-styled-components`,
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		`gatsby-plugin-transition-link`,
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images/`
+			}
+		},
+		{
+			resolve: `gatsby-source-contentful`,
+			options: {
+				spaceId: process.env.CONTENTFUL_SPACE_ID,
+				// Learn about environment variables: https://gatsby.dev/env-vars
+				accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+			}
+		}
+	]
+};
