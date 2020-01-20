ce5d5d7e6c1c195be49dc4a26ace770a596b4499
sarkanyjudo <sarkanyjudo@gmail.com>
2020-01-20 01:01:47 -1000

    Fixing issue with deploy

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { Helmet } from 'react';
+import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
+const getData = graphql`
+	query {
+		site {
+			siteMetadata {
+				siteTitle: title
+				siteDesc: description
+				author
+				siteUrl
+				image
+			}
+		}
+	}
+`;
 const SEO = ({ title, description }) => {
+	const { site } = useStaticQuery(getData);
+
+	const { siteDesc, siteTitle, siteUrl, image, twitterUsername } = site.siteMetadata;
+
 	return (
-		<Helmet title={title}>
-			<meta name="description" content={description} />
+		<Helmet htmlAttributes={{ lang: 'en' }} title={`${title} | ${siteTitle}`}>
+			<meta name="description" content={description || siteDesc} />
+			<meta name="image" content={image} />
 		</Helmet>
 	);
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,38 @@
-import React from "react"
-import AniLink from "gatsby-plugin-transition-link/AniLink";
-import Layout from '../components/Layout'
-import Banner from '../components/Banner'
-import About from '../components/Home/About'
-import Services from '../components/Home/Services'
-import StyledHero from '../components/StyledHero'
-import { graphql } from 'gatsby'
-import FeaturedTours from '../components/Home/FeaturedTours'
+import React from 'react';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import Layout from '../components/Layout';
+import Banner from '../components/Banner';
+import About from '../components/Home/About';
+import Services from '../components/Home/Services';
+import StyledHero from '../components/StyledHero';
+import { graphql } from 'gatsby';
+import FeaturedTours from '../components/Home/FeaturedTours';
+import SEO from '../components/SEO';
 
-export const query = graphql `
-    query {
-        defaultBcg: file(relativePath: {eq: "defaultBcg.jpeg"}) {
-        childImageSharp {
-            fluid(quality: 90,maxWidth: 4060) {
-            ...GatsbyImageSharpFluid_withWebp
-            }
-        }
-        }
-    }
-  
-`
+export const query = graphql`
+	query {
+		defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+			childImageSharp {
+				fluid(quality: 90, maxWidth: 4060) {
+					...GatsbyImageSharpFluid_withWebp
+				}
+			}
+		}
+	}
+`;
 
-export default ({data}) => (
-        <Layout>
-            <StyledHero home="true" img={data.defaultBcg.childImageSharp.fluid}>
-                <Banner title="continue exploring" info="Lorem ipsum dolor sit amet.">
-                    <AniLink fade to="/tours" className="btn-white">explore tours</AniLink>    
-                </Banner>
-            </StyledHero>
-            <About />
-            <Services />
-            <FeaturedTours />
-        </Layout>
-)
-
-
+export default ({ data }) => (
+	<Layout>
+		<SEO title="Home" />
+		<StyledHero home="true" img={data.defaultBcg.childImageSharp.fluid}>
+			<Banner title="continue exploring" info="Lorem ipsum dolor sit amet.">
+				<AniLink fade to="/tours" className="btn-white">
+					explore tours
+				</AniLink>
+			</Banner>
+		</StyledHero>
+		<About />
+		<Services />
+		<FeaturedTours />
+	</Layout>
+);
